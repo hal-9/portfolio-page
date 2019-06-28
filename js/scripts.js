dbf477c56aac90b243f5a5482f1701b6ca8a368a
Tuncay Aydin <tunikb@posteo.de>
2019-06-28 21:44:11 +0200

    first upload

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+window.addEventListener('scroll', function(e) {
+  /* When user scrolls, check position on the page */
+  if (window.scrollY !== 0) {
+    /* if page is scrolled, toggle class scrolled to reduce header size */
+    const header = document.getElementsByTagName('header');
+    header[0].classList.add('scrolled');
+  }
+
+  if (window.scrollY == 0) {
+    /* if page is not scrolled, remove scrolled class to default header */
+    const header = document.getElementsByTagName('header');
+    header[0].classList.remove('scrolled');
+  }
+})
