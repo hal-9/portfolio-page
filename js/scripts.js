1df19e0e1fc1afd05828ec39702a22a0cd7c6ef0
Tuncay Aydin <tunikb@posteo.de>
2019-09-23 16:54:52 +0200

    fix: fixed laziness of header animation

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,10 @@
 window.addEventListener('scroll', function(e) {
   /* When user scrolls, check position on the page */
-  if (window.scrollY !== 0) {
+  if (window.scrollY > 150) {
     /* if page is scrolled, toggle class scrolled to reduce header size */
     const header = document.getElementsByTagName('header');
     header[0].classList.add('scrolled');
-  }
-
-  if (window.scrollY == 0) {
+  } else {
     /* if page is not scrolled, remove scrolled class to default header */
     const header = document.getElementsByTagName('header');
     header[0].classList.remove('scrolled');
